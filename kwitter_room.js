c89cb2aa8c69846aa058b014ae89362e493a373e
TheCraziestGamer <86513102+TheCraziestGamer@users.noreply.github.com>
2021-10-21 19:48:05 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAxZEP_Pi4civ62bCQ8thtIlai2xcW6zA",
+    authDomain: "kwitter-28814.firebaseapp.com",
+    databaseURL: "https://kwitter-28814-default-rtdb.firebaseio.com",
+    projectId: "kwitter-28814",
+    storageBucket: "kwitter-28814.appspot.com",
+    messagingSenderId: "700516053813",
+    appId: "1:700516053813:web:cca36709f1582d67dfd27c"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
